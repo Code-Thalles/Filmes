67fadda15506d1be2e26c945387a546b8bffec16
Code-Thalles <santosthalles@outlook.com>
2023-06-30 13:48:18 -0300

    adicionando favicon

--- pag3.php
+++ pag3.php
@@ -29,6 +29,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/7926197/letter-y-clipart-md.png" type="image/x-icon">
     <title>Informações</title>
     <link rel="stylesheet" href="css/conteudo.css" type="text/css" media="screen">  
 </head>
